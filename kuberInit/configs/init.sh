e63c7e0d6c906c3ad4c9ce3a93983aac8f320d9c
Jiuglskiy.Vladislav <Jiuglskiy.Vladislav@chtp.net>
2021-03-22 21:46:26 +0300

    no message

--- kuberInit/configs/init.sh
+++ kuberInit/configs/init.sh
@@ -1,5 +1,6 @@
 kubectl apply -f dbase.yaml
-kubectl apply -f api-def.yaml
+kubectl apply -f api-default.yaml
 kubectl apply -f server-type.yaml
 kubectl apply -f redis-def.yaml
 kubectl apply -f rabbit-def.yaml
+kubectl apply -f builder-default.yaml
--- kuberInit/init.sh
+++ kuberInit/init.sh
@@ -2,3 +2,5 @@ cd mariadb && sh init.sh && cd ../
 cd rabbit && sh init.sh && cd ../
 cd redis && sh init.sh && cd ../
 cd api && sh init.sh && cd ../
+cd builder && sh init.sh && cd ../
+cd front && sh init.sh && cd ../
