b83623bcab7accdc54b28345324b35a178841f2f
Jiuglskiy.Vladislav <Jiuglskiy.Vladislav@chtp.net>
2021-03-24 22:28:48 +0300

    no message

--- initkuber.sh
+++ initkuber.sh
@@ -48,11 +48,10 @@ sudo cp /etc/fstab /etc/fstab_backup
 echo "____GITLAB____"
 kubectl apply -f gitlab-admin-service-account.yaml
 MY_NOW_IP=`curl -s https://icanhazip.com`
-MY_KUB_URL='http://'$MY_NOW_IP':6443'
-echo MY_KUB_URL
+sed -i "s/__KURL__/$MY_NOW_IP/" ./kuberInit/configs/server-type.yaml
+echo 'http://'$MY_NOW_IP':6443'
 kubectl config view --raw -o json | jq -r '.clusters[0].cluster."certificate-authority-data"' | tr -d '"' | base64 --decode
 MY_KUBE_TOKEN=`kubectl -n kube-system get secrets -o json | jq -r '.items[] | select(.metadata.name | startswith("gitlab-admin")) | .data.token' | base64 --decode`
-echo MY_KUBE_TOKEN
+sed -i "s/__KTOKEN__/$MY_KUBE_TOKEN/" ./kuberInit/configs/server-type.yaml
+echo $MY_KUBE_TOKEN
 echo "________"
-sed -i "s/__KURL__/$MY_KUB_URL/" ./kuberinit/configs/server-type.yaml
-sed -i "s/__KTOKEN__/$MY_KUBE_TOKEN/" ./kuberinit/configs/server-type.yaml
