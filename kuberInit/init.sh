ede4ad0ab20996150e431fa6769f1ed09195a214
Jiuglskiy.Vladislav <Jiuglskiy.Vladislav@chtp.net>
2021-03-24 22:06:09 +0300

    no message

--- kuberInit/init.sh
+++ kuberInit/init.sh
@@ -1,8 +1,8 @@
 cd mariadb && sh init.sh && cd ../
 cd rabbit && sh init.sh && cd ../
 cd redis && sh init.sh && cd ../
+cd wsswever && sh init.sh && cd ../
 cd api && sh init.sh && cd ../
+cd processor && sh init.sh && cd ../
 cd builder && sh init.sh && cd ../
 cd front && sh init.sh && cd ../
-cd wsswever && sh init.sh && cd ../
-cd processor && sh init.sh && cd ../
