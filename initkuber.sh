48f29e486f8c127cc1b21ebd73459045a304bca1
Jiuglskiy.Vladislav <Jiuglskiy.Vladislav@chtp.net>
2021-03-25 23:35:42 +0300

    no message

--- initkuber.sh
+++ initkuber.sh
@@ -48,6 +48,7 @@ sudo cp /etc/fstab /etc/fstab_backup
 echo "____GITLAB____"
 kubectl apply -f gitlab-admin-service-account.yaml
 MY_NOW_IP=`curl -s https://icanhazip.com`
+sed -i "s/__KUBEIP__/$MY_NOW_IP/" ./kuberInit/configs/server-type.yaml
 sed -i "s/__KURL__/$MY_NOW_IP/" ./kuberInit/configs/server-type.yaml
 echo `kubectl cluster-info | grep -E 'Kubernetes master|Kubernetes control plane' | awk '/http/ {print $NF}'`
 kubectl config view --raw -o json | jq -r '.clusters[0].cluster."certificate-authority-data"' | tr -d '"' | base64 --decode
